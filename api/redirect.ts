1fd83d3ff4d709c1d253434ddaba5633954b7055
Devon <100230169@student.burnley.ac.uk>
2026-01-28 22:00:22 +0000

    refactor 1

--- api/redirect.ts
+++ api/redirect.ts
@@ -10,12 +10,127 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader("Access-Control-Allow-Origin", "*"); // Or specific domain
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+
+  /*
+Check if GET request
+Check URL
+
+If fail - throw error
+
+Send file after above
+
+chechRequest
+Try Check req method -> if get, set type to GET in var
+Try url -> Get url and store in var
+
+Content type middleware - if working, set to html. else set to plaintext
+
+GET func -> Set ID to required
+
+
+
+  */
+let reqMethod = ''; // 'GET', 'POST' or 'OPTIONS'
+let reqUrl = ''; // 'index', 'testing'
+let validRequest = false;
+let fileData = ''
+
+const checkRequest = (req, res, next) => {
+
+    // method check
+
+    try{
+        if (req.method === 'GET') {
+            reqMethod = 'GET';
+        }
+        else if (req.method === 'POST') {
+            req.method = 'POST';
+        }
+        else if (req.method === 'OPTIONS') {
+            req.method = 'OPTIONS';
+        }
+        else {
+            throw new Error ('Invalid Method - use GET, POST or OPTIONS');
+        }
+    }
+    catch (error){
+        return error;
+    }
+
+    // url check
+
+    try{
+        if (req.url === '/') {
+            reqUrl = 'index';
+        }
+        else if (req.url === '/testing') {
+            reqUrl = 'testing';
+        }
+        else {
+            throw new Error ('404 Invalid URL - are you sure this is the correct address?');
+        }
+    }
+    catch (error) {
+        return error;
+    }
+    next();
+}
+
+const contentTypeMiddleware = (req, res, next) => {
+    if (reqUrl === 'GET') {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+    }
+    else if (reqMethod === 'POST' || reqMethod === 'OPTIONS') {
+        console.log(`Unsupported method ${reqMethod} used.`)
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+    }
+    next();
+}
+
+const readHTMLFile = (fileToGet) = {
+    
+}
+
+const testFunc = (fileToGet) = {
+    
+const getHTML = (req, res) => {
+    if (reqUrl === '/') {
+        res.write()
+    }
+    else if (reqUrl === '/testing') {
+
+    }
+    else {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   try{
         //check if GET request
         if (req.method === 'GET') {
             let filePath; // to choose what HTML to send back
             if (req.url === '/') { // if regular (no extension)
-                filePath = path.join(__dirname, '/../public/index.html') // starts at directory, then goes back and finds public, file
+                filePath = path.join('/../public/index.html')
             } else if (req.url === '/testing') { 
                filePath = path.join(__dirname, '/../public/testing.html')
             } else { // if neither source or about
