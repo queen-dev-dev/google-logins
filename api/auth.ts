8ff2c0f7ec723ada46daa0aa827d27c22f7e501d
Devon <100230169@student.burnley.ac.uk>
2026-02-05 10:46:07 +0000

    Some small fixes, transition to vercel

--- api/auth.ts
+++ api/auth.ts
@@ -1,21 +1,17 @@
-import http from "http";
 import { OAuth2Client } from "google-auth-library";
-import { URL } from "url";
 import crypto from "crypto"
 import * as cookie from 'cookie';
+import type { VercelRequest, VercelResponse} from '@vercel/node'
 
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const REDIRECT_URI = "http://localhost:3000/auth/google/callback";
+const REDIRECT_URI = process.env.VERCEL_URL
+  ?  `https://${process.env.VERCEL_URL}/api/auth?action=callback`
+  : "http://localhost:3000/api/auth?action=callback"
+;
 
 if (!CLIENT_ID || !CLIENT_SECRET) {
   throw new Error ("Missing Google OAuth env vars")
-}
-namespace NodeJS { // for compiler (less errors)
-  interface ProcessEnv {
-    GOOGLE_CLIENT_ID: string;
-    GOOGLE_CLIENT_SECRET: string;
-  }
 }
 
 const oauth2Client = new OAuth2Client(
@@ -25,10 +21,10 @@ const oauth2Client = new OAuth2Client(
 );
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const url = new URL(req.url, `http://${req.headers.host}`);
+  const {action} = req.query;
 
   //  Start Google login
-  if (url.pathname === "/auth/google") {
+  if (action === "login") {
     const state = crypto.randomUUID();
     const authUrl = oauth2Client.generateAuthUrl({
       scope: ["openid", "email", "profile"],
@@ -40,65 +36,58 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       "Set-Cookie",
       `oauth_state=${state}; HttpOnly; Path=/; SameSite=Lax`
     )
-    res.writeHead(302, { Location: authUrl });
-    res.end();
+    res.redirect(302, authUrl);
     return;
   }
 
   //  Google callback
-  if (url.pathname === "/auth/google/callback") {
-    const code = url.searchParams.get("code");
-    const returnedState = url.searchParams.get("state");
-    const cookies = cookie.parse(req.headers.cookie ?? "");
+  if (action === "callback") {
+    const code = req.query.code as string;
+    const returnedState = req.query.state as string;
+    const cookies = cookie.parse(req.headers.cookie as string ?? "");
     const storedState = cookies.oauth_state;
     
     if (!code) {
-      res.writeHead(400);
-      res.end("Missing auth code");
+      res.status(400).send("Missing auth code")
       return;
     }
 
     if (!returnedState || returnedState !== storedState) {
-      res.writeHead(403);
-      res.end("Invalid OAuth state")
+      res.status(403).send("Invalid OAuth state")
       return;
     }
     try {
       const { tokens } = await oauth2Client.getToken(code);
       oauth2Client.setCredentials(tokens);
+
       if (!tokens.id_token) throw new Error("NO ID token returned from Google")
+
       const ticket = await oauth2Client.verifyIdToken({
         idToken: tokens.id_token,
         audience: CLIENT_ID,
       });
 
       const payload = ticket.getPayload();
-
       if (!payload) throw new Error ("No token payload")
-      const googleUserId = payload.sub;
 
+      const googleUserId = payload.sub;
       const email = payload.email;
       const name = payload.name;
 
       // TODO: store googleUserId in DB
 
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
+      res.status(200).json({
           googleUserId,
           email,
           name,
-        })
-      );
+        });
     } catch (err) {
       console.error(err);
-      res.writeHead(500);
-      res.end("Authentication failed");
+      res.status(500).send("Authentication failed");
     }
     return;
   }
 
   // 404
-  res.writeHead(404);
-  res.end("Not found");
+  res.status(404).send("Not found");
 };
