20e895442fd72e7c166d438ab4b992ce42943c1c
Devon <100230169@student.burnley.ac.uk>
2026-02-12 10:25:04 +0000

    removed dotenv

--- api/script.js
+++ api/script.js
@@ -1,13 +1,11 @@
 import { ConvexHttpClient } from "convex/browser";
 import { api } from "../convex/_generated/api.js";
-import * as dotenv from "dotenv"
-dotenv.config({path: ".env.local"})
 
-const client = new ConvexHttpClient(process.env["CONVEX_URL"]);
+const client = new ConvexHttpClient(process.env.CONVEX_URL);
 
 export default async function () {
     try {
-        console.log("CONVEX_URL:", process.env["CONVEX_URL"]);
+        console.log("CONVEX_URL:", process.env.CONVEX_URL);
         console.log("Calling query...");
         const result = await client.query(api.tasks.get);
         console.log("Result:", result);
