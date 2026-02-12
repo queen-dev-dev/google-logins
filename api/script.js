0b31317ea0fc50c6cadc8dd166f6a6679df7b2ca
Devon <100230169@student.burnley.ac.uk>
2026-02-12 09:39:21 +0000

    Used dotenv to load env
    If you're wondering why there's so many commits, git is not installed at school

--- api/script.js
+++ api/script.js
@@ -1,7 +1,8 @@
 import { ConvexHttpClient } from "convex/browser";
-import { api } from "./convex/_generated/api.js";
+import { api } from "../convex/_generated/api.js";
+import * as dotenv from "dotenv"
+dotenv.config({path: ".env.local"})
 
-
-const client = new ConvexHttpClient(process.env.CONVEX_URL);
+const client = new ConvexHttpClient(process.env["CONVEX_URL"]);
 
 client.query(api.tasks.get).then(console.log);
