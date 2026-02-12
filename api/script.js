89efd832ec0a60018ee0b4920dd7fdcc7cb77723
Devon <100230169@student.burnley.ac.uk>
2026-02-12 09:30:55 +0000

    Update script.js

--- api/script.js
+++ api/script.js
@@ -2,6 +2,6 @@ import { ConvexHttpClient } from "convex/browser";
 import { api } from "./convex/_generated/api.js";
 
 
-const client = new ConvexHttpClient(process.env.CONVEX_URL);
+const client = new ConvexHttpClient(process.env.CONVEX_DEPLOY_KEY);
 
 client.query(api.tasks.get).then(console.log);
