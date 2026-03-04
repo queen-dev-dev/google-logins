5353f033209d52a1957d4b5265fcffbf05a523b8
Devon <100230169@student.burnley.ac.uk>
2026-03-04 09:43:51 +0000

    No longer inferred

--- api/auth.ts
+++ api/auth.ts
@@ -90,7 +90,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       const name = payload.name as string;
       const tokenExpiryDate = Date.now() + 2592000;
 
-      const allGoogleIDs = await convexClient.query(api.userLogin.getGoogleIDs); // array of numbers
+     const allGoogleIDs : number[] = await convexClient.query(api.userLogin.getGoogleIDs); // array of numbers
       for (let i = 0; i < allGoogleIDs.length; i++) {
         console.log(allGoogleIDs[i])
       }
